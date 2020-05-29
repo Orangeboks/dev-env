f28943cc18eee7b0451933cddd380d5909e232cf
cwetyyy <cvetina.todorova9702@abv.bg>
2020-05-29 17:23:28 +0200

    Front end update on viewes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,82 @@
 <template>
   <div class="about">
-    <h1>hjjjjjj</h1>
+<!-- Body -->
+    <div class="videos-view container">
+
+      <!-- courses-nav -->
+      <div class="courses-nav">
+        <ul class="">
+          <li><a class="current-program " href="#home">Web Development</a></li>
+          <li> <a href="#">User Experience</a> </li>
+          <li> <a href="#">Development environments</a> </li>
+          <li><a href="#home">Web programming</a></li>
+          <li><a href="#">Web - Data</a></li>
+          <li><a href="#">Web - Frontend</a></li>
+        </ul>
+      </div>
+
+      <h1 id="page-title">Development Environments</h1>
+
+      <!-- Videos grid -->
+      <div class="videos-grid">
+        <a class="video-link" href="#">
+          <div class="card mb-3" style="max-width: 700px;">
+            <div class="row no-gutters">
+              <div class="video-holder col-md-4">
+                <img src="img/test-pic.png" class="portrait card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Video title</h5>
+                  <p class="video-details card-text"><small class="text-muted" id="video-details">Victor Ky | Web Development | Fronend </small></p>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">120 views | 4 days ago | 40:10 <i class="fa fa-forward" aria-hidden="true"></i>
+                  </small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a class="video-link" href="#">
+          <div class="card mb-3" style="max-width: 700px;">
+            <div class="row no-gutters">
+              <div class="video-holder col-md-4">
+                <img src="img/test-pic.png" class="portrait card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Video title</h5>
+                  <p class="video-details card-text"><small class="text-muted" id="video-details">Victor Ky | Web Development | Fronend </small></p>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">120 views | 4 days ago | 40:10 <i class="fa fa-forward" aria-hidden="true"></i>
+                  </small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a class="video-link" href="#">
+          <div class="card mb-3" style="max-width: 700px;">
+            <div class="row no-gutters">
+              <div class="video-holder col-md-4">
+                <img src="img/test-pic.png" class="portrait card-img" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Video title</h5>
+                  <p class="video-details card-text"><small class="text-muted" id="video-details">Victor Ky | Web Development | Fronend </small></p>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">120 views | 4 days ago | 40:10 <i class="fa fa-forward" aria-hidden="true"></i>
+                  </small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+<!-- / Body -->
   </div>
 </template>
